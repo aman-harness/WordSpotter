6a616020c9d0550de364bd20477b03f17a1ddc3b
Aman <ug20131004@iitj.ac.in>
2017-04-18 14:41:32 +0530

    Allowed on all hosts

--- wordSpotter/settings.py
+++ wordSpotter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xwmi@a6egi7hbnbdq_+@p4j8hk%-s71ds7kt_8(d!rhyv_2v$u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
 
